7f2f430f023bfbd04abcd8f512834e0302b68721
bunnany <45374304+bunnany@users.noreply.github.com>
2019-08-14 11:07:08 +1200

    Create main routine that calls an initial function

---
+++ prototyping_exam_BNY.py
@@ -0,0 +1,12 @@
+##
+# prototyping_exam_BNY.py
+# Exam tracker prototype for AS91611
+# version 0.0
+
+def func_a():
+    """ Work to come here """
+    pass
+
+if __name__ == "__main__":
+    print("Hey friends, lets get started")
+    func_a()
